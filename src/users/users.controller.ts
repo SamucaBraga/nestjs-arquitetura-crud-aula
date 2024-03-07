15d0c7e57db454ba68e0cecb872976b9ffdfb2aa
Samuel Braga <samuelbgf@hotmail.com>
2024-03-07 14:51:25 -0300

    feat: implement endpoint delete user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,8 @@ import {
   Patch,
   Param,
   Delete,
+  HttpStatus,
+  HttpCode,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -36,7 +38,8 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    this.usersService.remove(+id);
   }
 }
