81ffc9bba31f6ba2d1318d9cc13ca8cd979a65fe
Samuel Braga <samuelbgf@hotmail.com>
2024-03-11 07:49:10 -0300

    feat: CRUD posts

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,26 +1,74 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Post, Prisma } from '@prisma/client';
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(dto: CreatePostDto): Promise<Post> {
+    const { authorEmail } = dto;
+
+    delete dto.authorEmail;
+
+    const data: Prisma.PostCreateInput = {
+      ...dto,
+      author: {
+        connect: {
+          email: authorEmail,
+        },
+      },
+    };
+
+    return this.prisma.post.create({
+      data,
+      include: {
+        author: true,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all posts`;
+  async findAll(): Promise<Post[]> {
+    return this.prisma.post.findMany({
+      include: {
+        author: true,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async getPublishedPosts(): Promise<Post[]> {
+    return this.prisma.post.findMany({
+      where: {
+        published: true,
+      },
+    });
   }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+  async findOne(id: number): Promise<Post | null> {
+    return this.prisma.post.findUniqueOrThrow({
+      where: { id },
+      include: {
+        author: true,
+      },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  async update(id: number, dto: UpdatePostDto): Promise<Post> {
+    const data: Prisma.PostUpdateInput = {
+      ...dto,
+    };
+
+    return this.prisma.post.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async remove(id: number): Promise<Post> {
+    return this.prisma.post.delete({
+      where: { id },
+    });
   }
 }
