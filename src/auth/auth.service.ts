cd49c5ae7c44d81a8ec96cdce67b44170fd8ac0d
Samuel Braga <samuelbgf@hotmail.com>
2024-03-13 09:01:38 -0300

    feat: implement authentication and authorization

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,52 @@
+import * as bcrypt from 'bcrypt';
+
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+
+import { UsersService } from 'src/users/users.service';
+
+import { UnauthorizedError } from './errors/unauthorized-error';
+
+import { UserToken } from './model/UserToken';
+import { UserPayload } from './model/UserPayload';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+  ) {}
+
+  async login(email: string, password: string): Promise<UserToken> {
+    const user: User = await this.validateUser(email, password);
+
+    const payload: UserPayload = {
+      username: user.email,
+      sub: user.id,
+    };
+
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
+
+  private async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (isPasswordValid) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+    }
+
+    throw new UnauthorizedError(
+      'Email address or password provided is incorrect.',
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { NotFoundInterceptor } from './users/interceptors/entity-not-found-interceptor';
+import { NotFoundInterceptor } from './users/interceptors/entity-not-found.interceptor';
+import { UnauthorizedInterceptor } from './auth/interceptors/unauthorized.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,7 @@ async function bootstrap() {
 
   // Interceptor
   app.useGlobalInterceptors(new NotFoundInterceptor());
+  app.useGlobalInterceptors(new UnauthorizedInterceptor());
 
   await app.listen(3000);
 }
