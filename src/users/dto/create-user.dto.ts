57033eab4313f50ca83f9a8f72bc4bd199cfe6c8
Samuel Braga <samuelbgf@hotmail.com>
2024-03-18 08:37:26 -0300

    docs: add ApiProperty

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -6,11 +6,20 @@ import {
   MinLength,
 } from 'class-validator';
 import { User } from '../entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto extends User {
+  @ApiProperty({
+    description: 'User name',
+    example: 'John Doe',
+  })
   @IsString()
   name: string;
 
+  @ApiProperty({
+    description: 'Password should be at least 8 characteres long',
+    example: 'Abc@123',
+  })
   @IsString()
   @MinLength(4)
   @MaxLength(20)
@@ -19,6 +28,10 @@ export class CreateUserDto extends User {
   })
   password: string;
 
+  @ApiProperty({
+    description: 'Email address',
+    example: 'johndoe@gmail.com',
+  })
   @IsEmail()
   email: string;
 }
